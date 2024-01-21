be3772436a4ef7c2c254a1c92476e9d60de33e4b
Neha_Kumari711 <nk8981398@gmail.com>
2024-01-21 23:03:40 +0530

    updated styling for buttons and images

---
+++ app.js
@@ -0,0 +1,13 @@
+ 
+ 
+ 
+ let typed= new Typed(".typing-1", {
+    strings: [
+        "Sigma- DSA + Web Development",
+        "Alpha - DSA",
+        "Delta - Web Development"
+    ],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+ });
